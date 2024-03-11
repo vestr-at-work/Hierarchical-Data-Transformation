df47310431c547a0d06f29dd6a74feca537a8298
vestr-at-work <petera-david@seznam.cz>
2024-03-11 21:04:13 +0100

    Add Ur constants

---
+++ src/main/java/cz/cuni/mff/hdt/ur/Ur.java
@@ -0,0 +1,16 @@
+package cz.cuni.mff.hdt.ur;
+
+public static class Ur {
+    public static final String KEY_TYPE = "@type";
+    public static final String KEY_VALUE = "@value";
+    public static final String KEY_CSV_ROWS = "@rows";
+    public static final String KEY_CSV_HEADER = "@header";
+    public static final String KEY_XML_VERSION = "@version";
+    public static final String KEY_XML_ENCODING = "@encoding";
+    public static final String KEY_XML_ATTRIBUTES = "@attributes";
+
+    public static final String VALUE_OBJECT = "object";
+    public static final String VALUE_STRING = "string";
+    public static final String VALUE_NUMBER = "number";
+    public static final String VALUE_BOOLEAN = "boolean";
+}
